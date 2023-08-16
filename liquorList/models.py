cf3fe13ff346cab57cb065937b105065a2781785
Coltenreq <finalrebellion123@gmail.com>
2023-08-16 02:52:05 -0500

    Changed to open source image, updated brand model with founding date.

--- liquorList/models.py
+++ liquorList/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 from django.core.validators import MinValueValidator
 
@@ -5,6 +6,7 @@ class Brand(models.Model):
     name = models.CharField(max_length=50, help_text="Enter the name of the liquor brand.")
     website = models.URLField(help_text="The URL of the brand's website.")
     country = models.CharField(max_length=30, help_text="The country of origin of the company.")
+    founded = models.DateField(default=datetime.date(2000, 1, 1))
 
 class Liquor(models.Model):
     name = models.CharField(max_length=50)
